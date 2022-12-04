ce4d9713ce5991fe17510605236631ccff2f7f34
yakimka <ss.yakim@gmail.com>
2022-12-04 13:57:21 +0200

    Day04 (#3)
    * Part 1
    
    * Part 2

---
+++ day04/part2.py
@@ -0,0 +1,68 @@
+from __future__ import annotations
+
+import os
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+class Section:
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+
+    @classmethod
+    def from_string(cls, string):
+        start, end = string.split('-')
+        return cls(int(start), int(end))
+
+    def __contains__(self, other: Section):
+        return self.start <= other.start <= other.end <= self.end
+
+    def has_intersections(self, other: Section):
+        return self.end >= other.start and self.start <= other.end
+
+
+def compute(s: str) -> int:
+    total = 0
+    for line in s.splitlines():
+        input1, input2 = line.split(",")
+        section1 = Section.from_string(input1)
+        section2 = Section.from_string(input2)
+        if section1.has_intersections(section2):
+            total += 1
+
+    return total
+
+
+INPUT_S = '''\
+2-4,6-8
+2-3,4-5
+5-7,7-9
+2-8,3-7
+6-6,4-6
+2-6,4-8
+'''
+EXPECTED = 4
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    with open(INPUT_TXT) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
