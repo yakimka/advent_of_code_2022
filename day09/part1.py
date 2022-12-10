44dedf0845cf7e60e5d202bb9d8e963596531461
yakimka <ss.yakim@gmail.com>
2022-12-10 12:29:06 +0200

    Part 1

--- day09/part1.py
+++ day09/part1.py
@@ -1,17 +1,51 @@
 from __future__ import annotations
 
 import os
+from enum import Enum
 
 import pytest
+
+from support import Vector2
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-def compute(s: str) -> int:
-    for line in s.splitlines():
-        pass
+class Direction(Enum):
+    UP = Vector2(0, -1)
+    RIGHT = Vector2(1, 0)
+    DOWN = Vector2(0, 1)
+    LEFT = Vector2(-1, 0)
 
-    return 0
+    @property
+    def opposite(self) -> Vector2:
+        vals = tuple(type(self).__members__.values())
+        return vals[(vals.index(self) + 2) % len(vals)]
+
+
+dmap = {
+    "U": Direction.UP,
+    "R": Direction.RIGHT,
+    "D": Direction.DOWN,
+    "L": Direction.LEFT,
+}
+
+
+def compute(s: str) -> int:
+    head = tail = Vector2(0, 0)
+    seen = {tail}
+
+    for line in s.splitlines():
+        dtype, steps = line.split()
+        move = dmap[dtype]
+        n = int(steps)
+
+        for _ in range(n):
+            head += move.value
+            if abs(head.x - tail.x) >= 2 or abs(head.y - tail.y) >= 2:
+                tail = move.opposite.value + head
+                seen.add(tail)
+
+    return len(seen)
 
 
 INPUT_S = '''\
@@ -30,7 +64,7 @@ EXPECTED = 13
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
     (
-        (INPUT_S, EXPECTED),
+            (INPUT_S, EXPECTED),
     ),
 )
 def test(input_s: str, expected: int) -> None:
--- support.py
+++ support.py
@@ -1,5 +1,6 @@
 from __future__ import annotations
 
+from dataclasses import dataclass
 from typing import Generator
 
 
@@ -9,3 +10,19 @@ def parse_numbers_split_new_line(s: str) -> Generator[int | None, None, None]:
             yield int(line)
         except ValueError:
             yield None
+
+
+@dataclass(frozen=True)
+class Vector2:
+    x: int
+    y: int
+
+    def __iter__(self):
+        return iter((self.x, self.y))
+
+    def __add__(self, other):
+        x, y = other
+        return type(self)(self.x + x, self.y + y)
+
+    def __radd__(self, other):
+        return self + other
