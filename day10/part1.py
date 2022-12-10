e5ba61c097d38d181cb7c79029be283b4894217b
yakimka <ss.yakim@gmail.com>
2022-12-10 21:37:40 +0200

    Part 1

---
+++ day10/part1.py
@@ -0,0 +1,209 @@
+from __future__ import annotations
+
+import os
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def noop():
+    yield None
+
+
+def addx(x_val: int):
+    yield None
+    yield x_val
+
+
+def pipeline(s: str):
+    for line in s.splitlines():
+        if line == "noop":
+            yield from noop()
+        if line.startswith("addx "):
+            yield from addx(int(line[5:]))
+
+
+def compute(s: str) -> int:
+    x = 1
+    answer = 0
+    signal_cycles = list(range(20, 221, 40))
+    for i, val in enumerate(pipeline(s), start=1):
+        if i in signal_cycles:
+            answer += i * x
+        if val is not None:
+            x += val
+
+    return answer
+
+
+INPUT_S = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+EXPECTED = 13140
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    with open(INPUT_TXT) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
