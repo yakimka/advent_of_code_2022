d4ec5d6b2076283cac940d16ec21e10f79e1c4bb
yakimka <ss.yakim@gmail.com>
2022-12-10 22:34:01 +0200

    Day 10 (#9)
    * Part 1
    
    * Part 2

---
+++ day10/part2.py
@@ -0,0 +1,226 @@
+from __future__ import annotations
+
+import os
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def noop():
+    yield 0
+
+
+def addx(x_val: int):
+    yield 0
+    yield x_val
+
+
+def read_instructions(s: str):
+    for line in s.splitlines():
+        if line == "noop":
+            yield from noop()
+        if line.startswith("addx "):
+            yield from addx(int(line[5:]))
+
+
+def cpu(instructions):
+    x = 1
+    for val in instructions:
+        yield x
+        x += val
+
+
+def screen():
+    for row_n in range(6):
+        for i in range(40):
+            yield row_n, i
+
+
+def compute(s: str) -> str:
+    surface = [[] for _ in range(6)]
+    for (crt_row, crt_i), x in zip(screen(), cpu(read_instructions(s))):
+        sprite_positions = [x - 1, x, x + 1]
+        draw_symbol = "#" if crt_i in sprite_positions else "."
+        surface[crt_row].append(draw_symbol)
+
+    return "\n".join(["".join(row) for row in surface])  # BUCACBUZ
+
+
+INPUT_S = '''\
+addx 15
+addx -11
+addx 6
+addx -3
+addx 5
+addx -1
+addx -8
+addx 13
+addx 4
+noop
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx 5
+addx -1
+addx -35
+addx 1
+addx 24
+addx -19
+addx 1
+addx 16
+addx -11
+noop
+noop
+addx 21
+addx -15
+noop
+noop
+addx -3
+addx 9
+addx 1
+addx -3
+addx 8
+addx 1
+addx 5
+noop
+noop
+noop
+noop
+noop
+addx -36
+noop
+addx 1
+addx 7
+noop
+noop
+noop
+addx 2
+addx 6
+noop
+noop
+noop
+noop
+noop
+addx 1
+noop
+noop
+addx 7
+addx 1
+noop
+addx -13
+addx 13
+addx 7
+noop
+addx 1
+addx -33
+noop
+noop
+noop
+addx 2
+noop
+noop
+noop
+addx 8
+noop
+addx -1
+addx 2
+addx 1
+noop
+addx 17
+addx -9
+addx 1
+addx 1
+addx -3
+addx 11
+noop
+noop
+addx 1
+noop
+addx 1
+noop
+noop
+addx -13
+addx -19
+addx 1
+addx 3
+addx 26
+addx -30
+addx 12
+addx -1
+addx 3
+addx 1
+noop
+noop
+noop
+addx -9
+addx 18
+addx 1
+addx 2
+noop
+noop
+addx 9
+noop
+noop
+noop
+addx -1
+addx 2
+addx -37
+addx 1
+addx 3
+noop
+addx 15
+addx -21
+addx 22
+addx -6
+addx 1
+noop
+addx 2
+addx 1
+noop
+addx -10
+noop
+noop
+addx 20
+addx 1
+addx 2
+addx 2
+addx -6
+addx -11
+noop
+noop
+noop
+'''
+EXPECTED = """\
+##..##..##..##..##..##..##..##..##..##..
+###...###...###...###...###...###...###.
+####....####....####....####....####....
+#####.....#####.....#####.....#####.....
+######......######......######......####
+#######.......#######.......#######.....\
+"""
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+            (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    with open(INPUT_TXT) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
