38c12e087e88ee31381b7cf3e4908c9dc63fdc78
yakimka <ss.yakim@gmail.com>
2022-12-13 01:42:11 +0200

    Part 1

--- day11/part1.py
+++ day11/part1.py
@@ -1,17 +1,79 @@
 from __future__ import annotations
 
+import operator
 import os
+from collections import deque
+from dataclasses import dataclass, field
+from functools import partial
+from typing import Callable
 
 import pytest
 
 INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
 
 
-def compute(s: str) -> int:
-    for line in s.splitlines():
-        pass
+@dataclass
+class Monkey:
+    index: int
+    operation: Callable[[int], int]
+    _divisor: int
+    _if_true_index: int
+    _if_false_index: int
+    items: deque[int] = field(default_factory=deque)
+    inspected: int = 0
 
-    return 0
+    def test(self, worry_level: int) -> bool:
+        return worry_level % self._divisor == 0
+
+    def get_next_index(self, worry_level: int) -> int:
+        if self.test(worry_level):
+            return self._if_true_index
+        return self._if_false_index
+
+
+def when_bored(level: int) -> int:
+    return level // 3
+
+
+operators = {
+    "*": operator.mul,
+    "+": operator.add,
+}
+
+
+def compute(s: str) -> int:
+    monkeys = []
+    kwargs = {}
+    for line in s.splitlines():
+        striped = line.strip()
+        if striped.startswith("Monkey "):
+            kwargs = {'index': int(striped[7:-1])}
+        elif striped.startswith("Starting items: "):
+            kwargs['items'] = deque(int(i) for i in striped[16:].split(", "))
+        elif striped.startswith("Operation: new = old "):
+            op_sign, value = striped[21:].split()
+            if value == "old":
+                kwargs['operation'] = lambda x, op_sign=op_sign: operators[op_sign](x, x)
+            else:
+                kwargs['operation'] = partial(operators[op_sign], int(value))
+        elif striped.startswith("Test: divisible by "):
+            kwargs['_divisor'] = int(striped[19:])
+        elif striped.startswith("If true: throw to monkey "):
+            kwargs['_if_true_index'] = int(striped[25:])
+        elif striped.startswith("If false: throw to monkey "):
+            kwargs['_if_false_index'] = int(striped[26:])
+
+            monkeys.append(Monkey(**kwargs))
+
+    for _ in range(20):
+        for monkey in monkeys:
+            while monkey.items:
+                worry_lvl = monkey.items.popleft()
+                new_lvl = when_bored(monkey.operation(worry_lvl))
+                monkeys[monkey.get_next_index(new_lvl)].items.append(new_lvl)
+                monkey.inspected += 1
+
+    return operator.mul(*sorted((m.inspected for m in monkeys))[-2:])
 
 
 INPUT_S = '''\
@@ -49,7 +111,7 @@ EXPECTED = 10605
 @pytest.mark.parametrize(
     ('input_s', 'expected'),
     (
-        (INPUT_S, EXPECTED),
+            (INPUT_S, EXPECTED),
     ),
 )
 def test(input_s: str, expected: int) -> None:
