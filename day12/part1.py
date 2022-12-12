3509f14f253967185c7f921bf012b68b52dbeef9
yakimka <ss.yakim@gmail.com>
2022-12-12 14:27:40 +0200

    Day 12

---
+++ day12/part1.py
@@ -0,0 +1,45 @@
+from __future__ import annotations
+
+import os
+
+import pytest
+
+INPUT_TXT = os.path.join(os.path.dirname(__file__), 'input.txt')
+
+
+def compute(s: str) -> int:
+    for line in s.splitlines():
+        pass
+
+    return 0
+
+
+INPUT_S = '''\
+Sabqponm
+abcryxxl
+accszExk
+acctuvwj
+abdefghi
+'''
+EXPECTED = 31
+
+
+@pytest.mark.parametrize(
+    ('input_s', 'expected'),
+    (
+        (INPUT_S, EXPECTED),
+    ),
+)
+def test(input_s: str, expected: int) -> None:
+    assert compute(input_s) == expected
+
+
+def main() -> int:
+    with open(INPUT_TXT) as f:
+        print(compute(f.read()))
+
+    return 0
+
+
+if __name__ == '__main__':
+    raise SystemExit(main())
